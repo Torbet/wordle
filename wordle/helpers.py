e676c76bee01dc0506d47f0e5e67a87a571c2580
Guy Torbet <guy@torbet.co>
2022-02-10 22:03:29 +0000

    better word list

--- wordle/helpers.py
+++ wordle/helpers.py
@@ -1,2 +1,8 @@
+def readFile(fileName):
+    fileObj = open(fileName, "r") #opens the file in read mode.
+    words = fileObj. read(). splitlines() #puts the file into an array.
+    fileObj. close()
+    return words
+
 def isValid(word):
    return True
